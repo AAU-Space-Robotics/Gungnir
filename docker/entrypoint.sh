e782550bd688fec623d674ba27ff519412bff028
pierre <pmorte22@student.aau.dk>
2026-02-17 18:58:26 +0100

    simple docker setup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-source /home/zed_ros2_ws/install/setup.bash
+#source /home/zed_ros2_ws/install/setup.bash #Pøllefar skal vi have den her?
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 echo "source /home/ws/install/setup.bash" >> ~/.bashrc
 
-echo "He he ho ho"
+echo "   ________                            .__         "
+echo "  /  _____/ __ __  ____    ____   ____ |__|______  "
+echo " /   \  ___|  |  \/    \  / ___\ /    \|  \_  __ \ "
+echo " \    \_\  \  |  /   |  \/ /_/  >   |  \  ||  | \/ "
+echo "  \______  /____/|___|  /\___  /|___|  /__||__|    "
+echo "         \/           \//_____/      \/            "
 
 bash
